ca1a8320874b3a0fbde89180312835b4c9af3334
rogerrodriguez27 <roger.rodriguez.27@unsch.edu.pe>
2024-05-03 18:06:35 -0500

    Codigo lab1

---
+++ Laboratorio 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola clase de Estructura de Datos");
+
+// sumar los n primeros numeros
+var n = 2;
+var suma = 0;
+for (var i = 1; i <= n; i++)
+{
+    suma += i;
+}
+
+Console.WriteLine($"la suma de los {n} numeros naturales son: {suma}");
